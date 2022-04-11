61306b30cd98306a9902bf2e91b7e90e5db04b22
William Owalla <william.owalla@student.moringaschool.com>
2022-04-11 11:39:15 +0300

    ft: add quote blue print

--- src/app/class/quote.ts
+++ src/app/class/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
+    displayQuote: boolean;
+    constructor (public quoteTitle: string,public author: string,public quote: string,public name: string,public id: number,public date: Date,public upVote: number,public downVote: number,){
+        this.displayQuote = false;
+    }
 }
