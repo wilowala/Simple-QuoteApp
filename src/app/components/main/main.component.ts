802316bebb6b8723c02e9211af931979e33d203e
William Owalla <william.owalla@student.moringaschool.com>
2022-04-11 11:37:50 +0300

    ft: instantiate quote array

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from 'src/app/class/quote';
 
 @Component({
   selector: 'app-main',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  quote: Quote[] = [
+    new Quote("Resilience","Sophie Kinsella", "There’s no such thing as ruining your life. Life’s a pretty resilient thing, it turns out.", "Wilie", 1, new Date(2022,1,17), 0, 0 ),
+    new Quote("Resilience","Sophie Kinsella", "There’s no such thing as ruining your life. Life’s a pretty resilient thing, it turns out.", "Wilie", 1, new Date(2022,2,6), 0, 0 ),
+    new Quote("Resilience","Sophie Kinsella", "There’s no such thing as ruining your life. Life’s a pretty resilient thing, it turns out.", "Wilie", 1, new Date(2022,2,10), 0, 0 ),
+    new Quote("Resilience","Sophie Kinsella", "There’s no such thing as ruining your life. Life’s a pretty resilient thing, it turns out.", "Wilie", 1, new Date(2022,2,26), 0, 0 ),
+    new Quote("Resilience","Sophie Kinsella", "There’s no such thing as ruining your life. Life’s a pretty resilient thing, it turns out.", "Wilie", 1, new Date(2022,3,12), 0, 0 ),
+    new Quote("Resilience","Sophie Kinsella", "There’s no such thing as ruining your life. Life’s a pretty resilient thing, it turns out.", "Wilie", 1, new Date(2022,4,17), 0, 0 )
+  ]
 
   constructor() { }
 
