59d57478815dd53420799d26fd1b8fa3d06388b7
William Owalla <william.owalla@student.moringaschool.com>
2022-04-12 07:04:59 +0300

    ft: add logig to the hidden details

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { Quote } from 'src/app/class/quote';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
+  @Input() quote:Quote;
+  @Output() isDeleted=new EventEmitter<boolean>();
+
+  quoteDeletion(remove:boolean){
+    this.isDeleted.emit(remove);
+  }
 
   constructor() { }
 
