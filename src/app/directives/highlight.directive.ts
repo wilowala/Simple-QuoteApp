192d1b4b1888e9dc5798f933f2fcb81bff21b1f6
William Owalla <william.owalla@student.moringaschool.com>
2022-04-12 07:08:19 +0300

    ft: create highlite directive

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) {
+    this.elem.nativeElement.style.backgroundColor="rgba(0, 0, 255,0.47)";
+   }
 
 }
