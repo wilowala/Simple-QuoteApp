26cea11167c2eb24cf463af377ec7d8a62852640
William Owalla <william.owalla@student.moringaschool.com>
2022-04-12 07:06:14 +0300

    ft: refacto form logic

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from 'src/app/class/quote';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
+  title = "Add a quote";
+  bttn1 ="Add quote";
+  bttn2 ="Clear form";
+  lable1 = "Quote tilte:";
+  lable2 = "Author:";
+  lable3 = "Quote:";
+  lable4 = "Your name:";
+  newQuote = new Quote ("","","","", 1, new Date(), 0, 0)
+  @Output() addQuote= new EventEmitter<Quote>();
+ 
+  onSubmit(form: NgForm){
+     let check=this.newQuote;
+     
+     this.addQuote.emit(this.newQuote);
+     form.resetForm(); 
+   }
   constructor() { }
 
   ngOnInit(): void {
