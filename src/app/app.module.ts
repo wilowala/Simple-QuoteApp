3ebf26b7ad6b66b82d6b15f1a6d6455aee1523c4
William Owalla <william.owalla@student.moringaschool.com>
2022-04-12 07:02:49 +0300

    ft: add forms module to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatePipe } from './pipe/date.pipe';
+import { MainComponent } from './components/main/main.component';
+import { FormComponent } from './components/form/form.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { HighlightDirective } from './directives/highlight.directive';
+import { DateCountPipe } from './pipe/date-count.pipe';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatePipe,
+    MainComponent,
+    FormComponent,
+    DetailComponent,
+    HighlightDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
